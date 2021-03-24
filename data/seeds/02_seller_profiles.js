e2c5bde384ca3c6961caa7304672da40c06bfb90
Allison Usher <allyjay317@gmail.com>
2021-03-23 20:05:16 -0700

    adjusted profile seeds

--- data/seeds/02_seller_profiles.js
+++ data/seeds/02_seller_profiles.js
@@ -7,6 +7,9 @@ const profiles = [...new Array(5)].map((i) => ({
   avatarUrl: faker.image.avatar(),
   email: faker.internet.email(),
   name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+  physical_address: faker.address(),
+  phone_number: faker.phone(),
+  role: 2,
 }));
 
 exports.seed = function (knex) {
@@ -15,30 +18,33 @@ exports.seed = function (knex) {
     ...profiles,
     {
       id: '00ulthapbErVUwVJy4x6',
-      seller_name: 'SuperStore Rug Emporium',
-      email_address: 'llama001@maildrop.cc',
+      name: 'SuperStore Rug Emporium',
+      email: 'llama001@maildrop.cc',
       phone_number: '(555) 444-3333',
       physical_address: '100 Davidson Street, Maine',
       description: 'Rugs, Rugs, Rugs! Discount Rugs, Fine Rugs, Bathroom Rugs!',
+      role: 1,
     },
 
     {
       id: '00ultwew80Onb2vOT4x6',
-      seller_name: 'Nicholas Mullen',
-      email_address: 'llama002@maildrop.cc',
+      name: 'Nicholas Mullen',
+      email: 'llama002@maildrop.cc',
       phone_number: '(555) 123-4567',
       physical_address: '35 Sampsonite Avenue, Lingdon Nevada, 22556',
       description:
         'Selling my personal Junk: Hawaiian shirts, mixed ammunition, unmatched fine china.',
+      role: 1,
     },
 
     {
       id: '00ultx74kMUmEW8054x6',
-      seller_name: "Bezo's Enterprises",
-      email_address: 'llama003@maildrop.cc',
+      name: "Bezo's Enterprises",
+      email: 'llama003@maildrop.cc',
       phone_number: '(555) 987-6543',
       physical_address: '1378 California Drive, California, 00012',
       description: 'Books, Rockets, Washington News Media',
+      role: 1,
     },
   ]);
 };
