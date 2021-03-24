8cafb63a79d30a96600d678a3fdf952ddb747ba6
Allison Usher <allyjay317@gmail.com>
2021-03-23 19:46:04 -0700

    created notifications and reference table

---
+++ data/migrations/20210323193341_notify.js
@@ -0,0 +1,35 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('notification_types', (tbl) => {
+      tbl.increments();
+      tbl.string('name').notNullable().unique();
+    })
+    .createTable('notifications', (tbl) => {
+      tbl
+        .string('user_id')
+        .notNullable()
+        .references('id')
+        .inTable('seller_profile')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      tbl
+        .integer('category_id')
+        .references('id')
+        .inTable('category')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      tbl
+        .string('seller_id')
+        .references('id')
+        .inTable('seller_profile')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      tbl.primary(['user_id', 'category_id', 'seller_id']);
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('notifications')
+    .dropTableIfExists('notification_types');
+};
