36dba113ad42fce73c24a1cf11eb18337782ef0e
patrick devincentis <patrickdevincentis@MacBook-Pro.local>
2021-03-31 21:52:15 -0400

    changes made to backend to get running on local machine

--- config/knexfile.js
+++ config/knexfile.js
@@ -2,14 +2,16 @@ var dotenv = require('dotenv');
 dotenv.config({ path: '../.env' });
 
 const pg = require('pg');
-pg.defaults.ssl = {
-  rejectUnauthorized: false,
-};
+// pg.defaults.ssl = {
+//   rejectUnauthorized: false,
+// };
 
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+    },
     migrations: { directory: '../data/migrations' },
     seeds: { directory: '../data/seeds' },
     pool: {
