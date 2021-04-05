10d46245541952997ecd188c77f9866409c6410a
Allison Usher <allyjay317@gmail.com>
2021-04-05 16:40:51 -0700

    created function to create item list

--- api/search/searchRouter.js
+++ api/search/searchRouter.js
@@ -1,7 +1,45 @@
 const router = require('express').Router();
 const testData = require('./testdata.json');
+const model = require('./searchModel');
+const authRequired = require('../middleware/authRequired');
 
-router.get('/', (req, res) => {});
+//mapbox
+const mbxClient = require('@mapbox/mapbox-sdk');
+const mbxGeocoder = require('@mapbox/mapbox-sdk/services/geocoding');
+
+const baseClient = mbxClient({ accessToken: process.env.MAPBOX_SECRET });
+const geocodeService = mbxGeocoder(baseClient);
+
+prepareItemList = async () => {
+  let items = await model.getItems();
+  items = await Promise.all(
+    items.map(
+      async (i) => await model.getItemCategories(i).then((newItem) => newItem)
+    )
+  );
+  items = await Promise.all(
+    items.map(async (i) => {
+      const geoRes = await geocodeService
+        .forwardGeocode({ query: i.physical_address, limit: 1 })
+        .send();
+      i.latitude = geoRes.body.features[0].center[0];
+      i.longitude = geoRes.body.features[0].center[1];
+      return i;
+    })
+  );
+  return items;
+};
+
+router.get('/', authRequired, async (req, res) => {
+  const profile_address = req.profile.physical_address;
+  const { title, category, address, zip } = req.query;
+  try {
+    let items = await prepareItemList();
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'a server error occured' });
+  }
+});
 
 router.get('/testdata', async (req, res) => {
   const { title } = req.query;
