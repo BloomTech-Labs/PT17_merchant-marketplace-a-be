01a69b32f874b2dc2f25f2464bae46f80b106b07
Allison Usher <allyjay317@gmail.com>
2021-04-13 20:26:00 -0700

    determined model functions

---
+++ api/ratings/ratingsModel.js
@@ -0,0 +1,20 @@
+const getProfileRatings = async (id) => {
+  return await knex('ratings as r')
+    .where({profile_id: id})
+    .join('item as i', 'r.item_id', '=', 'i.id')
+    .select('r.*', 'i.name')
+}
+const createRating = async (rating) => {
+  return await knex
+    .insert(rating)
+    .returning('*')
+}
+
+const editRating = async (profileId, itemId, newRating) => {
+  return await knex
+    .where({profile_id: profileId, item_id: itemId})
+    .update({rating: newRating}, ['profile_id', 'item_id', 'rating'])
+}
+
+
+module.exports = {getProfileRatings, createRating, editRating}
