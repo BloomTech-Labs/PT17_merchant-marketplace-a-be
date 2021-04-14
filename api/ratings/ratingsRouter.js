fa09fc2cf200591b807c06e73abe5c5d4f27a780
Allison Usher <allyjay317@gmail.com>
2021-04-13 21:00:31 -0700

    created routes for ratings

---
+++ api/ratings/ratingsRouter.js
@@ -0,0 +1,77 @@
+const router = require('express').Router();
+const authRequired = require('../middleware/authRequired');
+const db = require('./ratingsModel');
+const globalDb = require('../globalModel');
+
+router.get('/', authRequired, async (req, res) => {
+  const profileId = req.profile.id
+  try {
+    const ratings = await db.getProfileRatings(profileId);
+    res.status(200).json(ratings);
+  } catch (err) {
+    res.status(500).json({ message: 'a server error has occurred' });
+  }
+});
+
+router.post('/', authRequired, async (req, res) => {
+  const profileId = req.profile.id;
+  const { itemId, rating } = req.params;
+  if (!itemId || !rating) {
+    res
+      .status(400)
+      .json({ message: 'rating must include an itemId and a rating' });
+  } else {
+    try {
+      if (!checkIfItemExists(itemId)) {
+        res.status(404).json({ message: 'itemId does not exist' });
+      } else {
+        const newRating = await db.createRating({
+          profile_id: profileId,
+          item_id: itemId,
+          rating,
+        });
+        res.status(201).json(newRating);
+      }
+    } catch (err) {
+      res.status(500).json({ message: 'a server error has occurred' });
+    }
+  }
+});
+
+router.put('/:itemId', authRequired, (req, res) => {
+  const profileId = req.profile.id;
+  const { itemId, rating } = req.params;
+  try {
+    if (!checkIfItemExists(itemId)) {
+      res.status(404).json({ message: 'itemId does not exist' });
+    } else {
+      const newRating = db.editRating(profileId, itemId, rating)
+      res.status(200).json(newRating)
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'a server error has occurred' });
+  }
+});
+
+router.delete('/:itemId', authRequired, (req, res) => {
+  const profileId = req.profile.id;
+  const {itemId} = req.params
+  try{
+    const count = await db.deleteRating(profileId, itemId)
+    count > 0 ?
+      res.status(204)
+    :
+      res.status(404).json({message: 'could not find itemId'})
+    
+  } catch(err) {
+    res.status(500).json({ message: 'a server error has occurred' });
+  }
+});
+
+const checkIfItemExists = (itemId) => {
+  const item = globalDb.findById('item', itemId);
+  if (!item) {
+    return false;
+  }
+  return true;
+};
